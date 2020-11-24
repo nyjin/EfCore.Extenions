4c237d69bbd9f3710fc1fb2c8c55e2e8a9391dc2
노연진(Roh Yeonjin) <nyjin@ncsoft.com>
2020-11-24 21:03:49 +0900

    split folders and create test webapi project

--- EfCore.Extensions/RelaySpecification.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ardalis.Specification;
-
-namespace EfCore.Extensions
-{
-    public class RelaySpecification<T> : Specification<T>
-    {
-        public ISpecificationBuilder<T> GetQuery() => Query;
-    }
-}

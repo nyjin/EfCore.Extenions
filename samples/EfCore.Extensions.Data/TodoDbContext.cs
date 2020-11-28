bec753b5e8f4a8df1c2cb0dfc96d5b2d4921f380
Roh yeon jin <zacronan@naver.com>
2020-11-29 05:35:02 +0900

    feat: add trackableentity

--- samples/EfCore.Extensions.Data/TodoDbContext.cs
+++ samples/EfCore.Extensions.Data/TodoDbContext.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EfCore.Extensions.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +13,61 @@ namespace EfCore.Extensions.Data
 
         public DbSet<TodoItem> TodoItems { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            if(Database.IsSqlite())
+            {
+                foreach(var entity in modelBuilder.Model.GetEntityTypes())
+                {
+                    if(entity.ClrType.IsSubclassOf(typeof(TrackableEntity)))
+                    {
+                        modelBuilder.Entity(entity.ClrType)
+                            .Property<string>(nameof(TrackableEntity.CreatedBy))
+                            .HasDefaultValue("SYSTEM");
+
+                        modelBuilder.Entity(entity.ClrType)
+                            .Property<string>(nameof(TrackableEntity.UpdatedBy))
+                            .HasDefaultValue("SYSTEM");
+
+                        modelBuilder.Entity(entity.ClrType)
+                            .Property<DateTime>(nameof(TrackableEntity.CreatedAt))
+                            .ValueGeneratedOnAdd()
+                            .HasDefaultValueSql("datetime('now', 'localtime')");
+
+                        modelBuilder.Entity(entity.ClrType)
+                            .Property<DateTime>(nameof(TrackableEntity.UpdatedAt))
+                            .ValueGeneratedOnAdd()
+                            .HasDefaultValueSql("datetime('now', 'localtime')");
+                    }
+                }
+            }
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            foreach(var entity in this.ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified)
+                .Select(x => x.Entity)
+                .OfType<TrackableEntity>())
+            {
+                entity.UpdatedAt = DateTime.Now;
+            }
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach(var entity in this.ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified)
+                .Select(x => x.Entity)
+                .OfType<TrackableEntity>())
+            {
+                entity.UpdatedAt = DateTime.Now;
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
     }
 }
--- samples/EfCore.Extensions.Models/User.cs
+++ samples/EfCore.Extensions.Models/User.cs
@@ -1,14 +1,9 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EfCore.Extensions.Models
 {
-    public class User
+    public class User : TrackableEntity
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
         [Required]
         [MaxLength(128)]
         public string Name { get; set; }
